3fa4550821cb7bdb3cc997cd85c79eec22a01ed3
dbtop <berdaddario@gmail.com>
2022-03-24 21:00:18 -0300

    fix carrito button

--- js/app.js
+++ js/app.js
@@ -133,7 +133,11 @@ function carritoHTML() {
             <td class='margin ctd2'><strong>${precio}<strong></td>   
             <td class='margin ctd'>${cantidad}</td>  
             <td class='margin'>
-            <a href='#' class='borrar' data-id='${id}'> X </a>
+            <a href='#' class="borrar" data-id='${id}'>
+                <svg class="borrar2"  data-id='${id}' xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+            </a>
             </td>
             `;
         //Agrega el HTML en el tbody
@@ -159,7 +163,7 @@ function limpiarHTML() {
 
 function eliminarCurso(e){
 
-    if(e.target.classList.contains("borrar")){
+    if(e.target.classList.contains("borrar") || e.target.classList.contains("borrar2")){
         
         const cursoId = e.target.getAttribute('data-id');
         //elimina del arreglo por el data id con filter
