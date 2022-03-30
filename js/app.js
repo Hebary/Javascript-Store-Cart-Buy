4aed35727419575a93d987dc1a61610cf41d7096
dbtop <berdaddario@gmail.com>
2022-03-30 13:30:58 -0300

    feat:fetch+estilos

--- js/app.js
+++ js/app.js
@@ -55,6 +55,7 @@ function obtenerCursos(){
                     <a href="#" class="card__btn--agregar" data-id="${id}">Agregar Al Carrito</a>
                 </div>
                 `
+                
                 listaCursos.appendChild(card)
             })
         });
@@ -63,6 +64,7 @@ function obtenerCursos(){
 
 //agrega un curso al carrito si doy click sobre el btn que contiene la clase card__btn--agregar
 function agregarCurso(e) {
+
     e.preventDefault();
     if (e.target.classList.contains('card__btn--agregar')) {
         const cursoSeleccionado = e.target.parentElement.parentElement;
@@ -72,6 +74,7 @@ function agregarCurso(e) {
 }
 
 function leerCursoSeleccionado(curso) {
+
     const datosCurso = {
         nombre: curso.querySelector('.card__info>h4').textContent,
         precio: curso.querySelector('.card__precio').textContent,
@@ -105,6 +108,7 @@ function leerCursoSeleccionado(curso) {
 }
 
 function calcularPrecio(){
+
     let suma = 0;
         articulosCarrito.forEach(v_actual=>{
             suma += v_actual.precioNumber;
@@ -118,8 +122,8 @@ function calcularPrecio(){
 }
 
 function carritoHTML() {
-    limpiarHTML();
 
+    limpiarHTML();
     //recorre el carrito y genera el HTML
     articulosCarrito.forEach(curso => {
 
@@ -141,11 +145,7 @@ function carritoHTML() {
             <td><strong>${precio}<strong></td>   
             <td><strong>${cantidad}</strong></td>  
             <td>
-            <a href='#' class="borrar" data-id='${id}'>
-                <svg class="borrar2"  data-id='${id}' xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                </svg>
-            </a>
+                <a href='#' class="borrar" data-id='${id}'>X</a>
             </td>
             `;
         //Agrega el HTML en el tbody
@@ -160,7 +160,7 @@ function sincronizarStorage(){
 }
 
 
-//Elimina los cursos repetidos del tbody y luego carritoHTML regenera el HTML
+//impide los cursos repetidos en el carrito y luego carritoHTML  imprime el HTML
 
 function limpiarHTML() {
     while (listaCarrito.firstChild) {
@@ -171,7 +171,7 @@ function limpiarHTML() {
 
 function eliminarCurso(e){
 
-    if(e.target.classList.contains("borrar") || e.target.classList.contains("borrar2")){
+    if( e.target.classList.contains("borrar") ){
         
         const cursoId = e.target.getAttribute('data-id');
         articulosCarrito = articulosCarrito.filter( curso => curso.id !== cursoId );
